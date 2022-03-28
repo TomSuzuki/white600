04aa163ef701cd2ce1297667488e573ca275d047
TomSuzuki <tomsuzuki2633@gmail.com>
2022-03-28 22:47:55 +0900

    v1.0.0

--- mod_inline.go
+++ mod_inline.go
@@ -25,41 +25,20 @@ type inlineObject struct {
 func (data *MarkdownInfo) inlineConv(text string) string {
 
 	// 初期値の生成
-	inline := []inlineObject{
-		{
-			inlineType: inlineText,
-			content:    text,
-			child:      nil,
-		},
-	}
+	inline := []inlineObject{{inlineType: inlineText, content: text}}
 
-	// リンクの解析
+	// リンクの解析（画像リンクのためにデータを文字列に変換する）
 	inline = inlineLinkX(inline, [3]string{"![", "](", ")"}, "<img alt='$1' src='$2'>")
-	inline = inlineLinkX(inline, [3]string{"[", "](", ")"}, "<a href='$2'>$1</a>")
+	inline = inlineLinkX([]inlineObject{{inlineType: inlineText, content: inlineConvert(inline)}}, [3]string{"[", "](", ")"}, "<a href='$2'>$1</a>")
 
 	// 再帰的にその他のインライン要素の解析
 	inline = inlineTag(inline)
-
-	// // <img> and <a>
-	// data.inlineLink(inline, "![", "](", ")", "<img alt='$1' src='$2'>")
-	// data.inlineLink(inline, "[", "](", ")", "<a href='$2'>$1</a>")
-
-	// // inline text
-	// data.inlineTag(inline, "**", "strong")
-	// data.inlineTag(inline, "`", "code")
-	// data.inlineTag(inline, "~", "s")
-	// data.inlineTag(inline, "__", "em")
-	// data.inlineTag(inline, "_", "em")
-	// data.inlineTag(inline, "*", "em")
-
-	// <br>
-	//todo data.markdownLines[0] = strings.Replace(data.markdownLines[0], "  ", "<br>", -1)
 
 	// 解析結果を文字列に
 	return inlineConvert(inline)
 }
 
-// inlineLink ...インラインのリンクを解析（リンク内にリンクは作れないので再帰しない、リンク要素は前優先ではなく、字句優先）
+// inlineLink ...インラインのリンクを解析（リンク要素は前優先ではなく字句優先）
 func inlineLinkX(inline []inlineObject, mdTemplate [3]string, htmlTemplate string) []inlineObject {
 
 	// 全てのテキスト要素をチェック
@@ -260,105 +239,3 @@ func lexerInlineStart(text string, tag string) (bool, string, string) {
 	after := text[end+len(tag)*2:]
 	return true, inner, after
 }
-
-// ...インライン要素のタグを解析
-
-// // inlineLink ...![]() and []()
-// func (data *MarkdownInfo) inlineLink(start, middle, end string, format string) {
-// 	var line = data.currentData.currentLine
-// 	var p = 0
-
-// 	for true {
-// 		// index
-// 		var iMiddle = p + strings.Index(line[p:], middle)
-// 		if iMiddle == -1 {
-// 			break
-// 		}
-// 		var iStart = strings.LastIndex(line[:iMiddle], start)
-// 		var iEnd = iMiddle + strings.Index(line[iMiddle:], end)
-
-// 		// replace
-// 		if iMiddle != -1 && iStart != -1 && iEnd != -1 {
-// 			var s = strings.NewReplacer(
-// 				"$1", line[len(start)+iStart:iMiddle],
-// 				"$2", line[len(middle)+iMiddle:iEnd],
-// 			).Replace(format)
-// 			line = line[:iStart] + s + line[len(end)+iEnd:]
-// 		}
-
-// 		// error check
-// 		p = iMiddle + len(middle)
-// 		if p > len(line) {
-// 			break
-// 		}
-// 	}
-
-// 	//todo data.markdownLines[0] = line
-// }
-
-// // indexList
-// func indexList(s, substr string) []int {
-// 	var n []int
-// 	for true {
-// 		var m = strings.Index(s, substr)
-// 		if m == -1 {
-// 			break
-// 		}
-// 		n = append(n, m)
-// 		s = s[m:]
-// 	}
-// 	return n
-// }
-
-// // inlineTag ...md -> html
-// func (data *MarkdownInfo) inlineTag(md string, html string) {
-// 	var codeList = strings.Split(data.currentData.currentLine, md)
-// 	var isEven = len(codeList)%2 == 0
-// 	var text []string
-// 	data.currentData.currentLine = ""
-
-// 	// insert tags
-// 	for i, v := range codeList {
-// 		if isEven && i == len(codeList)-1 {
-// 			text = append(text, md)
-// 			text = append(text, v)
-// 		} else if i%2 == 0 {
-// 			text = append(text, v)
-// 		} else if isNotBrokenHTML(v) {
-// 			text = append(text, "<")
-// 			text = append(text, html)
-// 			text = append(text, ">")
-// 			text = append(text, v)
-// 			text = append(text, "</")
-// 			text = append(text, html)
-// 			text = append(text, ">")
-// 		} else {
-// 			text = append(text, md)
-// 			text = append(text, v)
-// 		}
-// 	}
-
-// 	// join
-// 	//todo data.markdownLines[0] = strings.Join(text, "")
-// }
-
-// // isNotBrokenHTML ..."<s></s><em></em>" <<< true, "<s><em></em>" <<< false, "<img ...>" <<< false...?
-// func isNotBrokenHTML(html string) bool {
-// 	var nest = 0
-// 	var open = false
-// 	for i := 0; i < len(html); i++ {
-// 		s := html[i]
-// 		if open {
-// 			if string(s) == "/" {
-// 				nest--
-// 			} else {
-// 				nest++
-// 			}
-// 		}
-// 		open = string(s) == "<"
-// 		if nest == -1 {
-// 			return false
-// 		}
-// 	}
-// 	return nest == 0
-// }
